5736bd1f92c437e4ce77a004d8ee4424ebcc8099
Jakub Janak <janakja5@fel.cvut.cz>
2024-05-07 13:50:41 +0200

    more tests were added and i am preparing for the TS semestralka to be finished, test and test scenarious have to be completed.

---
+++ src/test/java/IceManipulatorTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class IceManipulatorTest {
+
+    @Test
+    public void firstTest(){
+
+    }
+}
---
+++ src/test/java/ProcessTests.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+public class ProcessTests {
+
+    // The first process test will be the most important and will have the graph
+    // we can start the game and move on the map and collect the fruit and then win the game
+    @Test
+    public void processTest1() {
+
+    }
+
+    @Test
+    public void processTest2() {
+
+    }
+
+    @Test
+    public void processTest3() {
+
+    }
+
+    @Test
+    public void processTest4() {
+
+    }
+}

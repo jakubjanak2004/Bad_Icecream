c8924b640e9a886053d4313965f86bf07f28b996
Jakub Janak <janakja5@fel.cvut.cz>
2024-12-16 16:45:31 +0100

    added rotation painting to the Player

--- src/main/java/BoardElements/Player.java
+++ src/main/java/BoardElements/Player.java
@@ -13,6 +13,7 @@ public class Player extends BoardElement {
 
     /**
      * Will move the player on x-axis
+     *
      * @param xPosition x movement delta
      */
     public void moveOnx(int xPosition) {
@@ -21,6 +22,7 @@ public class Player extends BoardElement {
 
     /**
      * Will move the player on y-axis
+     *
      * @param yPosition y movement delta
      */
     public void moveOny(int yPosition) {
@@ -31,5 +33,23 @@ public class Player extends BoardElement {
     public void paint(Graphics2D g, int step, int widthPadding, int heightPadding) {
         g.setColor(getColor());
         g.fillOval(getXPosition() * step + widthPadding, getYPosition() * step + heightPadding, step, step);
+
+        // painting the rotation
+        g.setColor(Color.WHITE);
+        int firstOffset = (int) (3. / 8. * step);
+        int secondOffset = step / 15;
+
+        if (getRot() == Rotation.UP) {
+            g.fillOval(getXPosition() * step + widthPadding + firstOffset, getYPosition() * step + heightPadding + secondOffset, step / 4, step / 4);
+        }
+        if (getRot() == Rotation.LEFT) {
+            g.fillOval(getXPosition() * step + widthPadding + secondOffset, getYPosition() * step + heightPadding + firstOffset, step / 4, step / 4);
+        }
+        if (getRot() == Rotation.DOWN) {
+            g.fillOval(getXPosition() * step + widthPadding + firstOffset, getYPosition() * step + heightPadding + step - secondOffset - step / 4, step / 4, step / 4);
+        }
+        if (getRot() == Rotation.RIGHT) {
+            g.fillOval(getXPosition() * step + widthPadding + step - secondOffset - step / 4, getYPosition() * step + heightPadding + firstOffset, step / 4, step / 4);
+        }
     }
 }
